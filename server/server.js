593659e7fd9d74b85fff457c62c4f56bc31d6a7f
vsparrow <sparrowangels@gmail.com>
2017-05-23 19:02:25 +0000

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -13,7 +13,11 @@ app.use(express.static(publicPath)); //vreate app, configure middleware
 
 io.on("connection",(socket)=>{
     console.log("New user conneected");
-    // socket.emit("newMessage",{from:"user1",text:"I saw Rouge One",createdAt: 123})        
+    socket.emit("newMessage",{from: "Admin", text: "Welcome to the chat app",createdAt : new Date().getTime()})
+    socket.broadcast.emit("newMessage",{from: "Admin", text: "New user joined",
+        createdAt : new Date().getTime()
+    })
+
     socket.on("createMessage",(message)=>{
         console.log("createMessage",message)
         io.emit("newMessage",{
@@ -21,6 +25,11 @@ io.on("connection",(socket)=>{
             text : message.text,
             createdAt : new Date().getTime()
         })
+        // socket.broadcast.emit("newMessage",{
+        //     from: message.from,
+        //     text : message.text,
+        //     createdAt : new Date().getTime()
+        // })
     })
     socket.on("disconnect",()=>{console.log("User disconnected")})
 
