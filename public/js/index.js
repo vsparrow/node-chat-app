a4c8f222d698e15fbba5cd661b8944798244677c
vsparrow <sparrowangels@gmail.com>
2017-05-23 18:33:39 +0000

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ var socket = io();
 socket.on("connect",function(){
     console.log("Connected to server")
     // socket.emit("createEmail",{to : "jen@example.com",text: "Hey. This is Andrew" });
-    socket.emit("createMessage",{from: "chrome", text: "I use youtubee to buy videos"});
+    // socket.emit("createMessage",{from: "chrome", text: "I use youtubee to buy videos"});
 });
 socket.on("disconnect",function(){console.log("Disconnected from server")});
 // socket.on("newEmail",function(email){console.log("New email",email)});
